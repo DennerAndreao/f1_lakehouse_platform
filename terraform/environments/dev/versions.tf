6bd6820c10a21c72fb7001dd194125da7167f1fe
Denner Andreão <dennermandreao@gmail.com>
2026-05-18 08:56:27 -0300

    Configure remote S3 backend for dev state

--- terraform/environments/dev/versions.tf
+++ terraform/environments/dev/versions.tf
@@ -1,6 +1,13 @@
 ﻿terraform {
   required_version = ">= 1.15.0, < 2.0.0"
 
+  backend "s3" {
+    bucket       = "f1-lakehouse-terraform-state"
+    key          = "f1-lakehouse-platform/dev/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
+
   required_providers {
     databricks = {
       source  = "databricks/databricks"
